95d8dee1537621d5c142ba668d2c871802f9f5bc
Rob Hess <robwhess@gmail.com>
2020-05-18 15:54:37 -0700

    Add code from 5/18 lecture.

--- server.js
+++ server.js
@@ -1,13 +1,57 @@
 const express = require('express');
+const multer = require('multer');
+const crypto = require('crypto');
 
 const { connectToDB } = require('./lib/mongo');
-const { getImageInfoById } = require('./models/image');
+const { getImageInfoById, saveImageInfo } = require('./models/image');
 
 const app = express();
 const port = process.env.PORT || 8000;
 
+const imageTypes = {
+  'image/jpeg': 'jpg',
+  'image/png': 'png',
+  'image/gif': 'gif'
+};
+
+const upload = multer({
+  // dest: `${__dirname}/uploads`
+  storage: multer.diskStorage({
+    destination: `${__dirname}/uploads`,
+    filename: (req, file, callback) => {
+      const filename = crypto.pseudoRandomBytes(16).toString('hex');
+      const extension = imageTypes[file.mimetype];
+      callback(null, `${filename}.${extension}`);
+    }
+  }),
+  fileFilter: (req, file, callback) => {
+    callback(null, !!imageTypes[file.mimetype]);
+  }
+});
+
 app.get('/', (req, res, next) => {
   res.status(200).sendFile(__dirname + '/index.html');
+});
+
+app.post('/images', upload.single('image'), async (req, res, next) => {
+  console.log("== req.file:", req.file);
+  console.log("== req.body:", req.body);
+  if (req.file && req.body && req.body.userId) {
+    const image = {
+      contentType: req.file.mimetype,
+      filename: req.file.filename,
+      path: req.file.path,
+      userId: req.body.userId
+    };
+    const id = await saveImageInfo(image);
+    res.status(200).send({
+      id: id
+    });
+  } else {
+    res.status(400).send({
+      error: "Request body needs an 'image' file and 'userId' field."
+    });
+  }
 });
 
 app.get('/images/:id', async (req, res, next) => {
@@ -21,6 +65,13 @@ app.get('/images/:id', async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+});
+
+app.use('*', (err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({
+    error: "An error occurred.  Try again later."
+  });
 });
 
 app.use('*', (req, res, next) => {
