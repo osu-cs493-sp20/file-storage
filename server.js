25a3f576ee161e742eee2a18279477a4abf49536
Rob Hess <robwhess@gmail.com>
2020-05-20 15:56:25 -0700

    Add code from 5/20 lecture.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const crypto = require('crypto');
 
 const { connectToDB } = require('./lib/mongo');
-const { getImageInfoById, saveImageInfo } = require('./models/image');
+const { getImageInfoById, saveImageFile, getImageDownloadStreamByFilename } = require('./models/image');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -29,6 +29,8 @@ const upload = multer({
   }
 });
 
+// app.use("/media/images", express.static(`${__dirname}/uploads`));
+
 app.get('/', (req, res, next) => {
   res.status(200).sendFile(__dirname + '/index.html');
 });
@@ -43,7 +45,7 @@ app.post('/images', upload.single('image'), async (req, res, next) => {
       path: req.file.path,
       userId: req.body.userId
     };
-    const id = await saveImageInfo(image);
+    const id = await saveImageFile(image);
     res.status(200).send({
       id: id
     });
@@ -58,13 +60,36 @@ app.get('/images/:id', async (req, res, next) => {
   try {
     const image = await getImageInfoById(req.params.id);
     if (image) {
-      res.status(200).send(image);
+      // delete image.path;
+      // image.url = `/media/images/${image.filename}`;
+      const responseBody = {
+        _id: image._id,
+        url: `/media/images/${image.filename}`,
+        contentType: image.metadata.contentType,
+        userId: image.metadata.userId
+      };
+      res.status(200).send(responseBody);
     } else {
       next();
     }
   } catch (err) {
     next(err);
   }
+});
+
+app.get('/media/images/:filename', (req, res, next) => {
+  getImageDownloadStreamByFilename(req.params.filename)
+    .on('file', (file) => {
+      res.status(200).type(file.metadata.contentType);
+    })
+    .on('error', (err) => {
+      if (err.code === 'ENOENT') {
+        next();
+      } else {
+        next(err);
+      }
+    })
+    .pipe(res);
 });
 
 app.use('*', (err, req, res, next) => {
